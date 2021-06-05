ed5a30ea668054ee4bd3991df37a8c9960bd4d18
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-05 14:34:55 +0900

    Mt. Fuji

--- SwiftUI-Chapter3/ContentView.swift
+++ SwiftUI-Chapter3/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("mt-Fuji")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .scaleEffect(1.8)
+                .offset(x: 70, y: -30)
+                .frame(width: 300, height: 300)
+                .clipShape(Circle())
+                .shadow(radius: 20)
+                .overlay(
+                    Text("Hello, world!")
+                        .font(.title)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .offset(x:0, y: 10)
+                        .padding()
+                )
+        }
     }
 }
 
