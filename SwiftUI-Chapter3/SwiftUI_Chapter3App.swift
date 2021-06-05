6fa3a13f0cf9765ffb97fd7bf2d1f74ba63b1ff0
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-05 13:24:27 +0900

    Initial Commit

---
+++ SwiftUI-Chapter3/SwiftUI_Chapter3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter3App.swift
+//  SwiftUI-Chapter3
+//
+//  Created by 萩倉丈 on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
